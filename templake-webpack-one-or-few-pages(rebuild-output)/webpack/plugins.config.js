db4e357bc2a31940b321ad0ae487beffef0323dd
Boris Shagiev <boris260692@gmail.com>
2023-02-06 02:10:00 +0200

    Change WP config

--- templake-webpack-one-or-few-pages(rebuild-output)/webpack/plugins.config.js
+++ templake-webpack-one-or-few-pages(rebuild-output)/webpack/plugins.config.js
@@ -62,6 +62,24 @@ result.module = {
         'css-loader',
         'sass-loader']
     },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              "@babel/preset-env", {
+                "targets": {
+                  "node": "current"
+                }
+              }
+            ]
+          ]
+        }
+      }
+    }
   ]
 }
 
